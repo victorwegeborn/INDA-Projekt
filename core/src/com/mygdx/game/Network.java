71768c27638268cb552250fc27a08054f93b36a9
victorhv <victorhv@kth.se>
2015-05-13 11:45:32 +0200

    Kryonet 2.0

---
+++ core/src/com/mygdx/game/Network.java
@@ -0,0 +1,22 @@
+package com.mygdx.game;
+
+import com.esotericsoftware.kryonet.EndPoint;
+
+
+/** Network
+ * 
+ *  Class is used to keep things common between server and client
+ */
+
+public class Network {
+	
+	//STANDARD PORT
+	private static final int PORT = 54555;
+	
+	
+	//Register packages in the same order for all
+	//EndPoints (server and clients).
+	public static void register(EndPoint endpoint) {
+		
+	}
+}
