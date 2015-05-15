c421fbbc291a6af38fe64f911ba79f3b788f7b93
dhemgren <dhemgren@kth.se>
2015-05-15 15:09:55 +0200

    cleared unnecessary gradle dependencies

--- core/src/com/mygdx/game/CoordinateConverter.java
+++ core/src/com/mygdx/game/CoordinateConverter.java
@@ -2,22 +2,22 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * Quantizes body positions to the center
+ * of the nearest tile
+ * @author danhemgren
+ */
+
 public final class CoordinateConverter {
 	
 	public static Vector2 quantizePositionToGrid(Vector2 position){
-		//TODO: (x,y)-coordinates to grid coordinate conversion
+	
+		//Which tile is closest? Offsets position depending on distance to tile
+		float xOffset = position.x % 32 < 16f ? -0.5f : 0.5f;
+		float yOffset = position.y % 32 < 16f ? -0.5f : 0.5f;
 		
-		float x = position.x;
-		float y = position.y;
-		
-		//Which tile is this?
-//		x = x / 32;
-//		y = y / 32;
-		float xOffset = x % 32 < 16f ? -0.5f : 0.5f;
-		float yOffset = y % 32 < 16f ? -0.5f : 0.5f;
-		
-		x = Math.round(x + xOffset);
-		y = Math.round(y + yOffset);
+		float x = Math.round(position.x + xOffset);
+		float y = Math.round(position.y + yOffset);
 		
 		Vector2 finalpos = new Vector2(x, y);
 		
